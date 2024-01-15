2f02243139c2f2a51b2bafabc605ac805837c366
Rafael Coelho <rafaelcoelho3110@gmail.com>
2024-01-14 22:44:26 -0300

    feat: configurando log de exemplo para /is-alive

--- src/main/kotlin/com/tatame/IsAlive.kt
+++ src/main/kotlin/com/tatame/IsAlive.kt
@@ -1,5 +1,6 @@
 package com.tatame
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -7,7 +8,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/is-alive")
 class IsAlive {
+    val logger = LoggerFactory.getLogger(IsAlive::class.java)
 
     @GetMapping
-    fun isAlive():Boolean = true
+    fun isAlive():Boolean{
+        logger.info("Consulta /is-alive")
+        return true
+    }
 }
